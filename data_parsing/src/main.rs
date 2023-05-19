5c1cbe35946d31529915d227f61274b24088909d
Artemy Sazonov <90921891+AASazonov@users.noreply.github.com>
2023-05-19 21:02:31 +0300

    Cleaned the OrderBook methods: – OrderBook::from_file does not hold everything in RAM; – Added String as a possible input; – Removed duplicate code; – Drawback: too many calls for the OrderBook::update(...).

--- data_parsing/src/main.rs
+++ data_parsing/src/main.rs
@@ -2,18 +2,12 @@ mod orderbook;
 
 use crate::orderbook::{OrderBook};
 
-fn lines_from_string(s: &str) -> Vec<String>
-{
-    s.split('\n').map(str::to_string).collect()
-}
-
 fn main()
 {
-    let mut res: OrderBook = OrderBook::from_json("data/test.txt").unwrap();
-    OrderBook::print(&res);
+    let mut res = OrderBook::from_file("data/test.txt", 0.0025).unwrap();
+    res.print();
 
-    let string = "{\"date\":\"2022-11-10T07:00:03.124000004\",\"instrument\":\"USD/RUB_T+1\",\"type\":\"INCREMENT\",\"side\":\"BID\",\"quotes\":{\"added\":[],\"changed\":[],\"deleted\":[6153236882637504]}}\n{\"date\":\"2022-11-10T07:00:03.124000004\",\"instrument\":\"USD/RUB_T+1\",\"type\":\"INCREMENT\",\"side\":\"ASK\",\"quotes\":{\"added\":[{\"id\":1116150503,\"price\":161.505,\"size\":500000.0},{\"id\":16123234102,\"price\":161.11,\"size\":10000.0},{\"id\":161554102,\"price\":161.11,\"size\":30.0},{\"id\":1617685102,\"price\":161.11,\"size\":50000.0}],\"changed\":[{\"id\":6151503,\"price\":61.515,\"size\":56000.0}],\"deleted\":[6151503,615102,61507504,6150503]}}";
-    let lines = lines_from_string(string);
-    res.update(lines).unwrap();
-    OrderBook::print(&res);
+    let string = String::from("{\"date\":\"2022-11-10T07:00:03.124000004\",\"instrument\":\"USD/RUB_T+1\",\"type\":\"INCREMENT\",\"side\":\"BID\",\"quotes\":{\"added\":[],\"changed\":[],\"deleted\":[6153236882637504]}}\n{\"date\":\"2022-11-10T07:00:03.124000004\",\"instrument\":\"USD/RUB_T+1\",\"type\":\"INCREMENT\",\"side\":\"ASK\",\"quotes\":{\"added\":[{\"id\":1116150503,\"price\":161.505,\"size\":500000.0},{\"id\":16123234102,\"price\":161.11,\"size\":10000.0},{\"id\":161554102,\"price\":161.11,\"size\":30.0},{\"id\":1617685102,\"price\":161.11,\"size\":50000.0}],\"changed\":[{\"id\":6151503,\"price\":61.515,\"size\":56000.0}],\"deleted\":[6151503,615102,61507504,6150503]}}");
+    res.update_from_string(string).unwrap();
+    res.print();
 }
--- data_parsing/src/orderbook.rs
+++ data_parsing/src/orderbook.rs
@@ -1,7 +1,22 @@
 use std::collections::{BTreeMap, VecDeque,};
-use serde_json::{Value,};
-use std::{fs::File, io::{self, BufRead, BufReader}, path::Path, };
+use serde::{Deserialize, Serialize};
+use std::{io::{BufReader}, path::{Path}, };
+use std::fs::File;
+use std::io::{BufRead, Error};
+use std::vec::Vec;
 
+const BAD_STRING:          &str = "Bad string found. Check the inputs.";
+const BAD_PATH_TO_FILE:    &str = "File could not be found. Check the inputs.";
+const INSTRUMENT_MISMATCH: &str = "Instrument mismatch. Check the inputs.";
+
+
+pub enum Side
+{
+    BID,
+    ASK,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct L3Quote
 {
     pub id: i64,
@@ -9,11 +24,42 @@ pub struct L3Quote
     pub size: f64,
 }
 
-pub struct OrderBook // key = price/0.0025
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Quotes
+{
+    pub added: Vec<L3Quote>,
+    pub changed: Vec<L3Quote>,
+    pub deleted: Vec<i64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum QuotesEnum {
+    Quotes(Quotes),
+    Vec(Vec<String>)
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tick
+{
+    pub date: String,
+    pub instrument: String,
+    pub r#type: String,
+    pub side: String,
+    pub quotes: QuotesEnum,
+}
+
+pub struct OrderBook
 {
     pub instrument: String,
+    pub date: String,
+
     pub bid: BTreeMap<i64, VecDeque<L3Quote>>,
     pub ask: BTreeMap<i64, VecDeque<L3Quote>>,
+
+    price_step: f64,
+    price_step_inv: f64,
 }
 
 impl OrderBook
@@ -23,127 +69,152 @@ impl OrderBook
         BTreeMap::new()
     }
 
-    pub fn new() -> OrderBook
+    pub fn new(price_step: f64) -> OrderBook
     {
-        OrderBook{ instrument: "".to_string(), bid: OrderBook::new_side(), ask: OrderBook::new_side() }
+        OrderBook{ instrument: "".to_string(), date: "".to_string(), bid: OrderBook::new_side(), ask: OrderBook::new_side(), price_step, price_step_inv: 1./price_step }
     }
 
-    fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-        BufReader::new(File::open(filename)?).lines().collect()
-    }
-
-    pub fn update(&mut self, lines: Vec<String>) -> Result<&mut OrderBook, String>
+    pub fn update(&mut self, line: &String) -> Result<&mut OrderBook, String>
     {
-        for line in lines
+        let json_line: Tick = serde_json::from_str(&line).unwrap();
+
+        let side = if json_line.side == String::from("BID") { &mut self.bid } else { &mut self.ask };
+        if self.instrument == json_line.instrument || self.instrument == ""
         {
-            let json_line: Value = serde_json::from_str(&line).unwrap();
-
-            let json_line_side = &json_line["side"].to_string()[1..4];
-            let side = if json_line_side == "BID" { &mut self.bid } else { &mut self.ask };
-            if self.instrument != "" && self.instrument != json_line["instrument"].to_string()
+            if self.instrument == ""
             {
-                return Err("Wrong instrument".to_string());
+                self.instrument = json_line.instrument.to_string();
+            }
+            self.date = json_line.date;
+
+            let quotes = match json_line.quotes
+            {
+                QuotesEnum::Quotes(quotes) => quotes,
+                QuotesEnum::Vec(_) => {
+                    return Ok(self);
+                },
+            };
+            for quote in quotes.added
+            {
+                let price_key = (quote.price * self.price_step_inv) as i64;
+                side.entry(price_key).or_insert(VecDeque::new()).push_back(L3Quote {
+                    id: quote.id,
+                    price: quote.price,
+                    size: quote.size,
+                });
             }
 
-            let json_line_quotes = &json_line["quotes"];
-
-            let json_line_quotes_vec = json_line_quotes["added"].as_array();
-            if json_line_quotes_vec != None
+            for quote in quotes.changed
             {
-                let json_line_quotes_added = json_line_quotes_vec.unwrap();
-                for quote in json_line_quotes_added
+                let price_key = (quote.price * self.price_step_inv) as i64;
+                for (_key, value) in &mut *side
                 {
-                    let id = quote["id"].as_i64().unwrap();
-                    let price = quote["price"].as_f64().unwrap();
-                    let price_key = (price*400.0) as i64;
-                    let size = quote["size"].as_f64().unwrap();
-                    side.entry(price_key).or_insert(VecDeque::new()).push_back(L3Quote {
-                        id,
-                        price,
-                        size,
-                    });
+                    value.retain(|x| x.id != quote.id)
                 }
+                side.entry(price_key).or_insert(VecDeque::new()).push_back(L3Quote {
+                    id: quote.id,
+                    price: quote.price,
+                    size: quote.size,
+                });
             }
 
-            let json_line_quotes_vec = json_line_quotes["changed"].as_array();
-            if json_line_quotes_vec != None
+            for id in quotes.deleted
             {
-                let json_line_quotes_changed = json_line_quotes_vec.unwrap();
-                for quote in json_line_quotes_changed
+                for (_key, value) in &mut *side
                 {
-                    let id = quote["id"].as_i64().unwrap();
-                    let price = quote["price"].as_f64().unwrap();
-                    let price_key = (price*400.0) as i64;
-                    let size = quote["size"].as_f64().unwrap();
-                    for (_key, value) in &mut *side
-                    {
-                        value.retain(|x| x.id != id)
-                    }
-                    side.entry(price_key).or_insert(VecDeque::new()).push_back(L3Quote {
-                        id,
-                        price,
-                        size,
-                    });
-                }
-            }
-
-            let json_line_quotes_vec = json_line_quotes["deleted"].as_array();
-            if json_line_quotes_vec != None
-            {
-                let json_line_quotes_deleted = json_line_quotes_vec.unwrap();
-                for quote in json_line_quotes_deleted
-                {
-                    let id = quote.as_i64().unwrap();
-                    for (_key, value) in &mut *side
-                    {
-                        value.retain(|x| x.id != id);
-                    }
+                    value.retain(|x| x.id != id);
                 }
             }
         }
-        self.ask.retain(|_k,v| v.len() != 0);
-        self.bid.retain(|_k,v| v.len() != 0);
+        else
+        {
+            return Err(INSTRUMENT_MISMATCH.to_string());
+        }
 
         Ok(self)
     }
 
-    pub fn from_json(filename: &str) -> Result<OrderBook, String>
+    fn clean(&mut self)
     {
-        let mut orderbook = OrderBook::new();
+        self.ask.retain(|_k,v| v.len() != 0);
+        self.bid.retain(|_k,v| v.len() != 0);
+    }
 
-        if let Ok(lines) = OrderBook::lines_from_file(&filename)
+    pub fn update_from_string(&mut self, lines: String) -> Result<&mut OrderBook, String>
+    {
+        let lines: Vec<String> = lines.split("\n").map(|x| x.to_string()).collect();
+        for line in &lines
         {
-            orderbook.update(lines).expect("TODO: panic message");
+            self.update(line).expect(BAD_STRING);
         }
-        else
-        {
-            return Err("File could not be opened.".to_string());
-        }
+        self.clean();
+        Ok(self)
+    }
 
+    pub fn from_file(filename: &str, price_step: f64) -> Result<OrderBook, String>
+    {
+        let mut orderbook = OrderBook::new(price_step);
+        let path = Path::new(filename);
+        let display = path.display();
+
+        let file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", display, Error::to_string(&why)),
+            Ok(file) => file,
+        };
+        let reader = BufReader::new(file);
+        let lines = reader.lines();
+
+        for result in lines
+        {
+            if let Ok(line) = result
+            {
+                orderbook.update(&line).expect(BAD_PATH_TO_FILE);
+            }
+        }
+        orderbook.clean();
         Ok(orderbook)
     }
 
-    pub fn print(orderbook: &OrderBook)
+    pub fn from_str(json: &str, price_step: f64) -> Result<OrderBook, String>
     {
-        println!("\n\nAsk:");
-        for (key, value) in &orderbook.ask
-        {
-            let price_level: f64 = (*key as f64) * 0.0025;
-            print!("\nRUB {:.4}: ", price_level);
-            for quote in value {
-                print!("(id: {}, size: {}), ", quote.id, quote.size);
-            }
-        }
+        let mut orderbook = OrderBook::new(price_step);
+        let lines: Vec<String> = json.split("\n").map(|x| x.to_string()).collect();
 
-        println!("\n\nBid:");
-        for (key, value) in &orderbook.bid
+        for line in &lines
         {
-            let price_level: f64 = (*key as f64) * 0.0025;
-            print!("\nRUB {:.4}: ", price_level);
+            orderbook.update(line).expect(BAD_STRING);
+        }
+        orderbook.clean();
+        Ok(orderbook)
+    }
+
+    pub fn print_side(&self, _side: Side)
+    {
+        let side = match _side {
+            Side::ASK => &self.ask,
+            Side::BID => &self.bid,
+        };
+        println!("{}:", match _side {
+            Side::ASK => "ASK",
+            Side::BID => "BID",
+        });
+        for (key, value) in side
+        {
+            let price_level: f64 = (*key as f64) * self.price_step;
+            print!("RUB {:.4}: ", price_level);
             for quote in value {
                 print!("(id: {}, size: {}), ", quote.id, quote.size);
             }
+            print!("\n");
         }
-        println!("\n");
+        print!("\n");
+    }
+
+    pub fn print(&self)
+    {
+        println!("Order book for {} at {}", self.instrument, self.date);
+        self.print_side(Side::BID);
+        self.print_side(Side::ASK);
+        print!("\n");
     }
 }
