56997270301cf3b8bd346ab8b3051af2082d3b02
Artemy Sazonov <90921891+AASazonov@users.noreply.github.com>
2023-06-11 16:10:11 +0300

    Added a python module structure

---
+++ optexec/__init__.py
@@ -0,0 +1,6 @@
+from . import dataloader
+from . import tradingbot
+
+if __name__ == "__main__":
+    print("This is a module. Please, import it via the following command:\n\n> import optexec\n")
+    exit(-1)
