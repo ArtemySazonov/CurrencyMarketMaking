c6983beb3cfa9d0823aef08ad74efde9c81f8b04
DanilLegenkiy <legenkij2001@mail.ru>
2023-06-26 20:30:05 +0300

    dataloader implementation

--- optexec/dataloader.py
+++ optexec/dataloader.py
@@ -1,24 +1,30 @@
 import numpy as np
 
 class OnlineData:
-    def __create_price_by_volume__(self):
-        """Parses the data into the Callables
+    def __init__(self, filename):
+        self.filename = filename
+        self.f = open(filename, 'r')
 
-        Returns:
-            _type_: _description_
-        """
-        raise(NotImplementedError)
+    def __next__(self):
+        line = next(self.f)[:-1]
+        if len(line) < 1:
+          return None
+        Q_, P_  = np.array([z[1:-1].split(",") for z in line.split("\t")]).astype(float).astype(int).T
+        CS = np.cumsum(P_*np.ediff1d(Q_, to_begin=Q_[0]) )
         
+        def F(Q):
+          if Q <= Q_[0]:
+            return P_[0] * Q
+          if Q > Q_[-1]:
+            return None
+          i = np.searchsorted(Q_, Q)
+          return CS[i - 1] + (Q - Q_[i - 1])*P_[i] 
+        self.F = F
+        return self.F
+
+    def __iter__(self):
+        return self
+
+    def close(self):
+      self.f.close()
     
-    def __init__(self) -> None:
-        self.file = None
-        self.state = None
-
-        raise(NotImplementedError)
-    
-    def update(self) -> None:
-
-        raise(NotImplementedError)
-
-    def copy_state(self):
-        raise(NotImplementedError)
